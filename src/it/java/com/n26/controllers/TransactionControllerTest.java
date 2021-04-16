9e6e1f4ecb60af4959b7f4e68d39ce082fbfe776
Arefe <arefe@monetizenow.io>
2021-04-16 17:37:35 +0600

    tests are running now

--- src/it/java/com/n26/controllers/TransactionControllerTest.java
+++ src/it/java/com/n26/controllers/TransactionControllerTest.java
@@ -6,6 +6,25 @@ import org.junit.Before;
 import static org.junit.Assert.*;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 
 /**
  * Created by Chaklader on Apr, 2021
