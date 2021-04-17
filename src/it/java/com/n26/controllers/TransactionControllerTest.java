242ca367149aa96e5cb87972e803da8953275ddb
Arefe <arefe@monetizenow.io>
2021-04-17 07:31:09 +0600

    tests are running noww

--- src/it/java/com/n26/controllers/TransactionControllerTest.java
+++ src/it/java/com/n26/controllers/TransactionControllerTest.java
@@ -8,7 +8,6 @@ import com.n26.service.TransactionService;
 import com.n26.utls.ApiResponseMessage;
 import com.n26.utls.MessageConstant;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,15 +25,9 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.io.File;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Map;
-import java.util.UUID;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -226,65 +219,5 @@ public class TransactionControllerTest {
             .andExpect(MockMvcResultMatchers.content().json(expectedResponseString));
     }
 
-
-
-
-
-    /*
-     * this class provides the custom Utilities for the executing the transaction controller test
-     * */
-    private static class TransactionControllerTestUtils {
-
-
-        private static final UUID RANDOM_TRANSACTION_UUID = UUID.randomUUID();
-
-        public static String getCustomMessageForEntityFieldParsingException(String field, String fieldType) {
-
-            String result = null;
-
-            switch (fieldType) {
-
-                case "LOCAL_DATE_TIME" -> {
-
-                    result = "JSON parse error,  Cannot deserialize value of type `java.time.LocalDateTime` from String " + field;
-                }
-
-                case "AMOUNT" -> {
-
-                    result = "JSON parse error,  Cannot deserialize value of type `java.math.BigDecimal` from String " + field;
-                }
-
-                default -> {
-                    log.error("We dont have this field type in the transaction entity");
-                    return null;
-                }
-            }
-
-            return result;
-        }
-
-
-        public static String getFileContentWithLocation(String location) throws IOException {
-
-            String content = FileUtils.readFileToString(new File(location), StandardCharsets.UTF_8);
-
-            return content;
-        }
-
-
-        public static Transaction createTransactionWithProvidedData(Double amount, LocalDateTime minus) {
-
-            Transaction transaction = Transaction
-                                          .builder()
-                                          .amount(BigDecimal.valueOf(100.50).setScale(2, RoundingMode.HALF_UP))
-                                          .timestamp(minus)
-                                          .uuid(RANDOM_TRANSACTION_UUID)
-                                          .build();
-
-            log.info("created a new transaction using the provided data = " + transaction.toString());
-
-            return transaction;
-        }
-    }
 
 }
---
+++ src/main/java/com/n26/controllers/TransactionControllerTestUtils.java
@@ -0,0 +1,71 @@
+package com.n26.controllers;
+
+import com.n26.model.Transaction;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/*
+ * this class provides the custom Utilities for the executing the transaction controller test
+ * */
+@Slf4j
+public class TransactionControllerTestUtils {
+
+
+    private static final UUID RANDOM_TRANSACTION_UUID = UUID.randomUUID();
+
+    public static String getCustomMessageForEntityFieldParsingException(String field, String fieldType) {
+
+        String result = null;
+
+        switch (fieldType) {
+
+            case "LOCAL_DATE_TIME" -> {
+
+                result = "JSON parse error,  Cannot deserialize value of type `java.time.LocalDateTime` from String " + field;
+            }
+
+            case "AMOUNT" -> {
+
+                result = "JSON parse error,  Cannot deserialize value of type `java.math.BigDecimal` from String " + field;
+            }
+
+            default -> {
+                log.error("We dont have this field type in the transaction entity");
+                return null;
+            }
+        }
+
+        return result;
+    }
+
+
+    public static String getFileContentWithLocation(String location) throws IOException {
+
+        String content = FileUtils.readFileToString(new File(location), StandardCharsets.UTF_8);
+
+        return content;
+    }
+
+
+    public static Transaction createTransactionWithProvidedData(Double amount, LocalDateTime minus) {
+
+        Transaction transaction = Transaction
+                                      .builder()
+                                      .amount(BigDecimal.valueOf(100.50).setScale(2, RoundingMode.HALF_UP))
+                                      .timestamp(minus)
+                                      .uuid(RANDOM_TRANSACTION_UUID)
+                                      .build();
+
+        log.info("created a new transaction using the provided data = " + transaction.toString());
+
+        return transaction;
+    }
+}
